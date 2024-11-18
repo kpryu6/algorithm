af735c68fe750f23c69202aed40ce0a8b07382fb
Kyungpyo Ryu <113777043+kpryu6@users.noreply.github.com>
2024-11-18 11:41:23 +0900

    [Silver II] Title: 유기농 배추, Time: 48 ms, Memory: 31516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,58 +1,39 @@
-'''
-인접 노드간에는 지렁이 1 필요
-이거도 결국 그 그래픽에서 사이즈 구하는 문제
-'''
 import sys
-input = sys.stdin.readline
 sys.setrecursionlimit(100000)
-
-T = int(input().rstrip())
+input = sys.stdin.readline
 
 def dfs(x,y):
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
-    size = 1
 
-    visited[x][y] = True
+    if graph[x][y] == 1:
+        graph[x][y] = 0
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and graph[nx][ny] == 1:
-            size += bfs(nx,ny)
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                dfs(nx,ny)
 
-    return size
+        return True
 
-ans = list()
+    return False
 
+T = int(input().rstrip())
 for _ in range(T):
     M, N, K = map(int, input().split())
     graph = [[0] * M for _ in range(N)]
-    visited = [[False] * M for _ in range(N)]
+
     for _ in range(K):
         i, j = map(int, input().split())
         graph[j][i] = 1
 
-    warms = list()
+    cnt = 0
 
     for i in range(N):
         for j in range(M):
-            if not visited[i][j] and graph[i][j] == 1:
-                warms.append(dfs(i,j))
+            if dfs(i,j):
+                cnt += 1
 
-    ans.append(len(warms))
-
-for x in ans:
-    print(x)
-
-
-
-
-
-
-
-
-#for i in range(N):
-    #print(graph[i])
-
+    print(cnt)
