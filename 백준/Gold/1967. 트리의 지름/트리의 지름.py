c39bbf614cfdae261a2440c2d04c8331bcdc5f37
Kyungpyo Ryu <113777043+kpryu6@users.noreply.github.com>
2024-11-30 12:04:57 +0900

    [Gold IV] Title: 트리의 지름, Time: 76 ms, Memory: 35668 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,56 +1,32 @@
-'''
-트리에 존재하는 모든 경로들 중에서 가장 긴 것의 길이
-root에서 leaf까지의 길이?
-경로 하나하나 다 check?
-
-root -> leaf 길이 다 구해서
-가장 긴거 2개
-공통경로 빼면 되지 않나?
-공통 경로는 어떻게 구할건데
-
-이게 아님
-
-풀이
-1. 시작 정점에서 임의의 정점까지 거리 구해서 가장 먼 거리 구함
-2. 1에서 찾은 가장 먼 거리를 시작 정점으로 하여 다시 한번 가장 긴 거리 찾기
-'''
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10**9)
 
-# 일단 경로 저장
 T = defaultdict(list)
-N = int(input().rstrip())
+N = int(input())
+q = deque()
 for _ in range(N-1):
-    s, e, cost = map(int, input().rstrip().split())
-    T[s].append((e, cost))
-    T[e].append((s, cost))
+    s, e, d = map(int, input().split())
+    T[s].append((e,d))
+    T[e].append((s,d))
 
-# print(T)
+def bfs(start):
 
-visited = [-1] * (N+1)
-visited[1] = 0
+    visited = [-1] * (N+1)
+    visited[start] = 0
+    q.append(start)
 
-# 1번 구하기(DFS)
-def dfs(start, d):
-    for next, next_d in T[start]:
-        if visited[next] == -1:
-            visited[next] = d + next_d
-            dfs(next, visited[next])
+    while q:
+        cur = q.popleft()
+        for next, next_d in T[cur]:
+            if visited[next] == -1:
+                visited[next] = visited[cur]+ next_d
+                q.append(next)
 
-dfs(1,0)
+    long_from_root = visited.index(max(visited))
 
-# 2번 구하기
-long_from_root = visited.index(max(visited))
+    return [long_from_root, max(visited)]
 
-visited = [-1] * (N+1)
-visited[long_from_root] = 0
-dfs(long_from_root, 0)
-
-print(max(visited))
-
-
-
+print(bfs(bfs(1)[0])[1])
 
 
