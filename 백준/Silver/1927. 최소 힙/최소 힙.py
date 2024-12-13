80ed5cb69df8c561370a212f55b25defd0ea2b6f
Kyungpyo Ryu <113777043+kpryu6@users.noreply.github.com>
2024-12-13 22:07:09 +0900

    [Silver II] Title: 최소 힙, Time: 116 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,27 @@
+'''
+x가 자연수면,
+배열에 x 추가
+
+x가 0이면,
+배열에서 가장 작은 값 출력하고 그 값 제거(pop)
+
+입력에서 0이 주어진 횟수만큼 답을 출력한다
+
+-> 우선순위 큐 사용하자
+
+'''
+import sys
+import heapq
+input = sys.stdin.readline
+li = []
+N = int(input())
+for _ in range(N):
+    num = int(input())
+    if num > 0:
+        heapq.heappush(li, num)
+    elif num == 0:
+        if len(li) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(li))
+
