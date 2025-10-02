7d321b981e0909bd05277492e77e95cc36460811
Kyungpyo Ryu <113777043+kpryu6@users.noreply.github.com>
2025-10-02 20:03:07 +0900

    [level 1] Title: 폰켓몬, Time: 1.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,23 @@
 '''
-종류 최대한 많이 선택해야 함
+4C2
+최대한 많은 종류 폰켓몬 N/2마리 선택
+
+1번
+
+2번 
+1. 일단 6/2 -> 3
+2. 여기 key 개수
+    3: 3개
+    2: 2개
+    4: 1개
+1번과 2번 개수 중 작은거
+
+3번
+1. 일단 6/2 -> 3
+
+3: 3개
+2: 3개
 '''
 def solution(nums):
-    return min(len(set(nums)), len(nums)/2)
+    return min(len(nums)/2,len(set(nums)))
+    
