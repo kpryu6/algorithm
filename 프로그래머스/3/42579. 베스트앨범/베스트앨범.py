4895c2cffb8c6a65c51b1f941436d431550c88ec
Kyungpyo Ryu <113777043+kpryu6@users.noreply.github.com>
2025-10-13 15:44:33 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,37 +1,42 @@
 '''
-많이 재생된 노래 2개씩 모아
-재생 높은 순
-1. 장르
-2. 노래
-3. 고유번호 낮은 노래 먼저 수록
+genre 별 베스트 2개씩 -> 베스트 엘범
 
-베스트 앨범 노래 고유번호 순서대로 return
+1. 노래 많은 장르 먼저
+2. 장르 안에서 재생높은거 먼저
+3. 재생횟수 같으면 번호 낮은거 먼저
+
+plays = 노래 별 재생 횟수
+
+보기에는 고유번호는 index 인듯
+return 베스트 앨범에 들어갈 노래 번호들 순서대로
+
+defaultdict(<class 'list'>, {'classic': [(0,500), (2,150), (3,800)], 'pop': [600, 2500]})
+
+장르 100개 미만이라는디;;
 '''
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    from collections import defaultdict, Counter
-    best = defaultdict(list)
-    play_cnt = Counter()
+    music = defaultdict(list)
+    for i,(x,y) in enumerate(zip(genres, plays)):
+        music[x].append((i,y))
     
-    
-    for i in range(len(genres)):
-        play_cnt[genres[i]] += plays[i]
-        best[genres[i]].append(i)
-    
-    # print(play_cnt)
-    # print(best)
-        
-    # 장르 정렬
-    sorted_genre = sorted(play_cnt, key=lambda x: -play_cnt[x])
-    # print(sorted_genre)
-    
-    # 장르에 따른 노래 정렬
+    # 어느 장르가 더 많이 재생됐는지 - 장르 정렬
+    # 〉	['pop', 'classic']
+    sorted_genre = sorted(
+        list(music.keys()),
+        key=lambda x: sum(map(lambda y: y[1], music[x])),
+        reverse=True)
+
+    # 그 장르에서 많이 재생된거는 뭔지
+    # 재생 횟수 같은거 중 번호 낮은거 먼저
     for genre in sorted_genre:
-        sorted_play = sorted(
-            best[genre],
-            key=lambda x: (-plays[x], x)
-        )
-        # print(sorted_play)
-        answer.extend(sorted_play[:2])
+        temp = [e[0] for e in sorted(music[genre],key=lambda x: (-x[1], x[0]))]
+        answer += temp[:2]
+    
     
     return answer
+
+
+
